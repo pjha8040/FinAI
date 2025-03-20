7434b4dd400aa06db6458d1b7dac713052023cf7
pratik pankaj <pratikpankaj04@gmail.conm>
2025-03-20 17:02:14 +0530

    Resolve Backend

--- finAi/backend/server.js
+++ finAi/backend/server.js
@@ -1,32 +1,18 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const cors = require("cors");
-require("dotenv").config();
-// const connectDB = require("./config/db");
 
-// import routes
-// example:
-// const adminRoutes = require("./routes/adminRoutes");
-// const userRoutes = require("./routes/userRoutes");
-// const courseRoutes = require("./routes/courseRoutes");
-// const authRoutes = require("./routes/authRoutes");
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
+app.use(cors());
 
-// connect Database
-// connectDB();
+// Routes
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 
-//Routes
-// app.use("/admin", adminRoutes);
-// app.use("/", userRoutes);
-// app.use("/courses", courseRoutes);
-// app.use("/auth", authRoutes);
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
